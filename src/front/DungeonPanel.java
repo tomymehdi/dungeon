a491319ffeed0b33e13da4d8aa341c8abe444466
Tomas Alfredo Mehdi <tmehdi@labu69.unix.lab>
2011-05-27 18:45:53 -0300

    front engahned

--- src/front/DungeonPanel.java
+++ src/front/DungeonPanel.java
@@ -1,20 +1,26 @@
 package front;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Image;
 
 import back.Floor;
 import back.Monster;
 import back.Putable;
+import back.Wall;
 
 public class DungeonPanel extends GamePanel {
 
 	private static final long serialVersionUID = 1L;
 	private static final int CELL_SIZE = 30;
 
+	private Image[][] secondLayerImages;
+	private Integer rowSecondLayer;
+	private Integer colSecondLayer;
+
 	public DungeonPanel(DungeonGameFrame dungeonGameFrame) {
-		super(dungeonGameFrame.game.getBoardDimension().x - 2,
-				dungeonGameFrame.game.getBoardDimension().y - 2, CELL_SIZE,
+		super(dungeonGameFrame.game.getBoardDimension().y - 2,
+				dungeonGameFrame.game.getBoardDimension().x - 2, CELL_SIZE,
 				new GamePanelListener() {
 
 					@Override
@@ -24,30 +30,63 @@ public class DungeonPanel extends GamePanel {
 					}
 				}, Color.BLUE);
 
+		rowSecondLayer = dungeonGameFrame.game.getBoardDimension().x - 2;
+		colSecondLayer = dungeonGameFrame.game.getBoardDimension().y - 2;
+
+		secondLayerImages = new Image[rowSecondLayer][colSecondLayer];
+
 		drawDungeon(dungeonGameFrame);
 		this.setVisible(true);
 	}
 
 	private void drawDungeon(DungeonGameFrame dungeonGameFrame) {
 		Image image;
-		for (int i = 1; i < dungeonGameFrame.game.getBoardDimension().x-1; i++) {
-			for (int j = 1; j < dungeonGameFrame.game.getBoardDimension().y-1; j++) {
+		for (int i = 1; i < dungeonGameFrame.game.getBoardDimension().x - 1; i++) {
+			for (int j = 1; j < dungeonGameFrame.game.getBoardDimension().y - 1; j++) {
 				Putable cell = dungeonGameFrame.game.getBoard()[i][j];
-				System.out.println(cell);
-				System.out.println((i-1) + "," + (j-1));
+				image = dungeonGameFrame.getBoardImagesByClass().get(
+						Floor.class);
+				put(image, i - 1, j - 1);
 				if (cell.getClass().equals(Monster.class)) {
-					image = dungeonGameFrame.getBoardImagesByClass().get(Floor.class);
-					put(image, i-1, j-1);
 					image = dungeonGameFrame.getMonsterImagesByName().get(
 							((Monster) cell).getMonsterType().toString());
+					putSecondLayer(image, i - 1, j - 1);
 				} else {
 					image = dungeonGameFrame.getBoardImagesByClass().get(
 							cell.getClass());
+					if (cell.getClass().equals(Wall.class)) {
+						put(image, i - 1, j - 1);
+					} else {
+						putSecondLayer(image, i - 1, j - 1);
+					}
 				}
-
-				this.put(image, j - 1, i - 1);
 			}
+		}
+		putSecondLayer(dungeonGameFrame.getPlayerImage(), dungeonGameFrame.game
+				.getPlayer().getPosition().x - 1, dungeonGameFrame.game
+				.getPlayer().getPosition().y - 1);
+	}
 
+	private void putSecondLayer(Image image, int i, int j) {
+		secondLayerImages[i][j] = image;
+	}
+
+	@Override
+	public void put(Image image, int row, int colum) {
+		super.put(image, colum, row);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+
+		for (int i = 0; i < rowSecondLayer; i++) {
+			for (int j = 0; j < colSecondLayer; j++) {
+				if (secondLayerImages[i][j] != null) {
+					g.drawImage(secondLayerImages[j][i], i * CELL_SIZE, j
+							* CELL_SIZE, null);
+				}
+			}
 		}
 	}
 
