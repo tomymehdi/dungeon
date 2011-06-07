cd9c38ef34aff9b7481e81463988c21cab81ac0b
Tomas Alfredo Mehdi <tmehdi@labu15.unix.lab>
2011-06-07 09:22:34 -0300

    no changes

--- src/front/LevelSelectorImp.java
+++ src/front/LevelSelectorImp.java
@@ -2,6 +2,8 @@ package front;
 
 import java.awt.Frame;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -19,15 +21,24 @@ public class LevelSelectorImp extends JFrame implements LevelSelector {
 
 	public LevelSelectorImp(Frame frameToShowOn) {
 
-		String[] listBoards;
+		String[] auxFiles, listBoardsShowed;
+		List<String> listBoards = new ArrayList<String>();
 		File directory = new File("." + File.separator + "boards");
-		listBoards = directory.list();
-		for(int i = 0 ; i < listBoards.length ; i++){
-			listBoards[i] = listBoards[i].replace(".board", "");
+		auxFiles = directory.list();
+		for (String s : auxFiles) {
+			if (s.endsWith(".board")) {
+				listBoards.add(s.replace(".board", ""));
+			}
 		}
+		listBoardsShowed = new String[listBoards.size()];
+		for (int k = 0; k < listBoards.size(); k++) {
+			listBoardsShowed[k] = listBoards.get(k);
+		}
+
 		Object levelSelected = JOptionPane.showInputDialog(frameToShowOn,
 				"Select level", "Levels selector",
-				JOptionPane.QUESTION_MESSAGE, null, listBoards, listBoards[0]);
+				JOptionPane.QUESTION_MESSAGE, null, listBoardsShowed,
+				listBoardsShowed[0]);
 		if (levelSelected != null) {
 			this.levelSelected = new File("." + File.separator + "boards"
 					+ File.separator + levelSelected + ".board");
